5c54d2cd73bbde3e514d8d3ff8cbe5f5d5f5714d
Chithra <chitra-venkat@expertdigitalweb.com>
2024-02-29 19:56:03 +0530

    scrolltoptobottom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+ import ScrollToTopAndBottom from './scroll-to-top-and-bottom';
+// import ScrollToSection from './scroll-to-top-and-bottom/scroll-to-section';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <ScrollToTopAndBottom/>
+
+      <br>
+      </br> 
+      {/* <ScrollToSection/> */}
+     
     </div>
   );
 }
